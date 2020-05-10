72f5840b3fc50cf1cefeb10678815755b7f752bd
Doug Wigginton <dougwig@gmail.com>
2020-05-10 01:08:06 -0400

    Updates

---
+++ precache-manifest.1c958f65a556fc226d8337b3246e31a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aba300f9c3510ff42019a17e79b0a1c",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "50df9d4c5794ce42673b",
+    "url": "/react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9662ebf8fdaa54da8318",
+    "url": "/react-app/static/js/2.9a5a9f82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.9a5a9f82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50df9d4c5794ce42673b",
+    "url": "/react-app/static/js/main.6d6f51b5.chunk.js"
+  },
+  {
+    "revision": "2859edfa3cf6df56101c",
+    "url": "/react-app/static/js/runtime-main.b41e6653.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
